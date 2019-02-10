535d76f905a445fc6f8cf7d4850bbb1cb377183b
František Puciow <ozzac22@gmail.com>
2019-02-11 00:49:38 +0100

    První commit

---
+++ RPG_Game/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfAnimatedGif;
+
+namespace RPG_Game
+{
+    /// <summary>
+    /// Interakční logika pro App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
