d7bb533c662b1b66552b07638ebffb8aac6becc4
František Puciow <ozzac22@gmail.com>
2019-02-23 19:03:43 +0100

    Oprava chyb způsobených Gitem
    Oprava chyb způsobených Gitem

---
+++ RPG__Game/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using RPG_Game.pages;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RPG_Game
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Frame frame;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            frame = myFrame;
+
+        }
+
+
+    }
+}
